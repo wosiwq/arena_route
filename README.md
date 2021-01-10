697259f8d4e340a0811caae7ffddbaeac68f99e7
wosiwq <z_weipeng@126.com>
2021-01-10 15:32:27 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# arena_route
-计算 公主连结 竞技场/公主竞技场 最优击剑路径（大概）
+# 如何安装
+在*\hoshino\modules文件夹内，按住shift之后右键文件夹内空的地方，选择 `在此处打开powershell 窗口`，接着在窗口内输入 `git clone https://github.com/wosiwq/arena_route.git` ，最后在 `__bot__.py` 中添加arena_route 即可。
+
+# 如何使用
+
+只有一条命令
+**击剑路径 排名**
+
+> 示例：击剑路径 15001
+
+空格**很重要**
+
+# 注意
+
+此module默认不启用，启用请在群内发送 `启用 击剑路径计算`
+
+想要默认启用，请将第五行的 `enable_on_default=False` 删除
+
+想要回复的消息不at本人，请将21行的 `at_sender=True` 改为 `at_sender=False`
---
+++ arena_route.py
@@ -0,0 +1,21 @@
+import math
+from hoshino import Service
+
+
+sv = Service("击剑路径计算", enable_on_default=False)
+
+@sv.on_rex(r'击剑路径 (.{0,5})$')
+async def arena_route(bot,ev):
+    num=int(ev['match'].group(1))
+    r={}
+    rank = 0
+    while num>1:
+        rank += 1
+        if (num<=11):
+            num = 1
+        elif (num<69):
+            num -= 10
+        else:
+            num = math.floor(num*0.85)
+        r[rank] = num
+    await bot.send(ev,"最优击剑路径(大概)："+','.join(map(str,list(r.values())[:5])),at_sender=True)
