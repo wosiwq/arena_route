0a25572b6113e9b8434f02a6d7ad504c9b56e498
wosiwq <z_weipeng@126.com>
2021-05-15 18:56:49 +0800

    正则修改

--- arena_route.py
+++ arena_route.py
@@ -4,9 +4,9 @@ from hoshino import Service
 
 sv = Service("击剑路径计算", enable_on_default=False)
 
-@sv.on_rex(r'击剑路径 (.{0,5})$')
+@sv.on_rex(r'击剑(路径|路线)( )?(.{0,5})$')
 async def arena_route(bot,ev):
-    num=int(ev['match'].group(1))
+    num=int(ev['match'].group(3))
     r={}
     rank = 0
     while num>1:
